a7493a7d54141342287bc3de87c1487d9d282b68
Omar Gonzalez <om.gonzalez007@gmail.com>
2017-11-03 16:32:24 -0500

    Add files via upload

---
+++ Positivo_Negativo.py
@@ -0,0 +1,12 @@
+
+print('Introduzca un numero')
+
+a = int(input())
+if a > 0:
+    print('Es un numero positivo')
+elif a == 0:
+    print('El numero es igual a 0')
+elif a < 0:
+    print('El numero es negativo')
+
+
